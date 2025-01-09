ee5fa4de17535483b48560475e4cb9051aa6a7c0
chogyejin2 <chogyejin@spendit.kr>
2025-01-09 18:40:04 +0900

    displayMarker 유틸 함수 추가

---
+++ src/shared/lib/map.ts
@@ -0,0 +1,26 @@
+export const displayMarker = (
+  map: any,
+  locPosition: { La: number; Ma: number },
+  message: string
+) => {
+  // 마커를 생성합니다
+  const marker = new window.kakao.maps.Marker({
+    map: map,
+    position: locPosition,
+  });
+
+  const iwContent = message;
+  const iwRemoveable = true;
+
+  // 인포윈도우를 생성합니다
+  const infowindow = new window.kakao.maps.InfoWindow({
+    content: iwContent,
+    removable: iwRemoveable,
+  });
+
+  // 인포윈도우를 마커위에 표시합니다
+  infowindow.open(map, marker);
+
+  // 지도 중심좌표를 접속위치로 변경합니다
+  map.setCenter(locPosition);
+};
