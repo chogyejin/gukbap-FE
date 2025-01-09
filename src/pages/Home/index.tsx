2109d571ae317523f26929b6f77d517028303ec7
chogyejin2 <chogyejin@spendit.kr>
2025-01-09 18:40:24 +0900

    Home 페이지 현 위치로 맵 띄우기

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,36 @@
+import { useEffect, useRef } from 'react';
+import styles from './Home.module.css';
+import { displayMarker } from '@/shared/lib/map';
+
 export const Home = () => {
-  return <div>Home</div>;
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const kakao = window.kakao;
+    const container = mapRef.current;
+
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+
+    const map = new kakao.maps.Map(container, options);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        const locPosition = new window.kakao.maps.LatLng(lat, lon);
+
+        displayMarker(map, locPosition, '현 위치');
+      });
+    } else {
+      const locPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+
+      displayMarker(map, locPosition, '현 위치');
+    }
+  }, []);
+
+  return <div ref={mapRef} className={styles.map} />;
 };
