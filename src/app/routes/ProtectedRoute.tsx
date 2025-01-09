76e0ecae9e0869ed51a04048ea39b2eeee986e24
chogyejin2 <chogyejin@spendit.kr>
2025-01-09 18:39:31 +0900

    ProtectedRoute 리팩토링

--- src/app/routes/ProtectedRoute.tsx
+++ src/app/routes/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
-import { isAuthenticated } from '@/shared/lib/auth';
+import { isAuthenticated } from '@/shared/lib/storage';
 import { Navigate } from 'react-router';
 
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
+  return isAuthenticated('token') ? children : <Navigate replace to="/login" />;
 };
